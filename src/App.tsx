26fda85c2d9536de42c11f11d8ea709b0d8f8f6a
Adolfo Reyes <mktic.test@gmail.com>
2026-07-21 15:00:18 -0600

    ✨Feature(App): Implementación de TanStack básico

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,46 @@
+import { useQuery } from '@tanstack/react-query';
+
 import './App.css';
 
 const App = () => {
+    const { data, error, isFetching, isPending, refetch } = useQuery({
+        queryKey: [''],
+        queryFn: async () => {
+            const response = await fetch(
+                'https://www.random.org/integers/?num=1&min=1&max=500&col=1&base=10&format=plain&rnd=new',
+            );
+            return response.json();
+        },
+        // Evita realizar la petición cuando se hace focus en la vista
+        refetchOnWindowFocus: false,
+    });
+
+    if (isPending) {
+        return <h1>Cargando...</h1>;
+    }
+
+    if (error) {
+        return <h1>`Error ${error.message}`</h1>;
+    }
+
     return (
-        <>
-            <h1>Hola mundo</h1>
-        </>
+        <div
+            style={{
+                display: 'flex',
+                alignItems: 'center',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                height: '100vh',
+            }}>
+            <h1>`Hola mundo...${data}`</h1>
+            {isFetching && <p>Actualizando...</p>}
+            <button
+                style={{ padding: '12px', width: '120px' }}
+                onClick={() => refetch()}
+                disabled={isFetching}>
+                Actualizar
+            </button>
+        </div>
     );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,20 @@
 import { StrictMode } from 'react';
 
 import { createRoot } from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+import App from './App.tsx';
 
 import './index.css';
-import App from './App.tsx';
+
+const queryCient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <App />
+        <QueryClientProvider client={queryCient}>
+            <ReactQueryDevtools client={queryCient} />
+            <App />
+        </QueryClientProvider>
     </StrictMode>,
 );
