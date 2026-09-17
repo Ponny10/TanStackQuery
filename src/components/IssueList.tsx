86d4a11be3a31c62f0278a92113fa1709cf55b64
Adolfo Reyes <mktic.test@gmail.com>
2026-09-16 23:02:02 -0600

    ✨Feature(Components): Se actualizan componentes IssueComment, IssueItem, IssueList y LabelPicker con data dinámica

--- src/components/IssueList.tsx
+++ src/components/IssueList.tsx
@@ -1,21 +1,26 @@
+import type { GithubIssue } from '../models/issues.type';
 import { IssueItem } from './IssueItem';
 
-export const IssueList = () => {
-  return (
-    <>
-      {/* Botones de All, Open, Closed */}
-      <div className="flex gap-4">
-        <button className="btn active">All</button>
-        <button className="btn">Open</button>
-        <button className="btn">Closed</button>
-      </div>
+type IssueListProps = {
+    issues: GithubIssue[];
+};
 
-      {/* Lista de issues */}
-      <div className="mt-4">
-        {[1, 2, 3].map((issue) => (
-          <IssueItem key={issue} />
-        ))}
-      </div>
-    </>
-  );
+export const IssueList = ({ issues }: IssueListProps) => {
+    return (
+        <>
+            {/* Botones de All, Open, Closed */}
+            <div className="flex gap-4">
+                <button className="btn active">All</button>
+                <button className="btn">Open</button>
+                <button className="btn">Closed</button>
+            </div>
+
+            {/* Lista de issues */}
+            <div className="mt-4">
+                {issues.map((issue) => (
+                    <IssueItem key={issue.id} issue={issue} />
+                ))}
+            </div>
+        </>
+    );
 };
--- src/components/LabelPicker.tsx
+++ src/components/LabelPicker.tsx
@@ -1,22 +1,20 @@
-import { useQuery } from '@tanstack/react-query';
-import { getLabels } from '../store/actions/getLabels.actions';
+import { useLabels } from '../hooks/useLabels';
+import { LoadingSpinner } from '../shared/LoadingSpinner';
 
 export const LabelPicker = () => {
-    const labelsQuery = useQuery({
-        queryKey: ['labels'],
-        queryFn: getLabels,
-    });
+    const { labelsQuery } = useLabels();
 
     if (labelsQuery.isLoading) {
-        return <h1>Obteniendo datos, espere un momento...</h1>;
+        return <LoadingSpinner />;
     }
+
     return (
         <div className="flex flex-wrap gap-2 justify-center">
             {labelsQuery.data?.map((label) => (
                 <span
                     key={label.id}
-                    className="px-2 py-1 rounded-full text-xs font-semibold hover:bg-slate-800 cursor-pointer"
-                    style={{ border: `1px solid #${label.color}`, color: 'white' }}>
+                    className="animate-fade-in px-2 py-1 rounded-full text-xs font-semibold hover:bg-slate-800 cursor-pointer"
+                    style={{ border: `1px solid #${label.color}` }}>
                     {label.name}
                 </span>
             ))}
